1c4d2ff5d11164259aa844a9bdf10dd85a73eeac
Tomek28611 <tomek286@yahoo.com>
2022-11-26 06:47:43 +0100

    All admin pages added, template ready

--- frontend/src/pages/admin/AdminChatsPage.js
+++ frontend/src/pages/admin/AdminChatsPage.js
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import AdminLinksComponent from "../../components/admin/AdminLinksComponent";
+import AdminChatRoomComponent from "../../components/admin/AdminChatRoomComponent";
+import { Row, Col } from "react-bootstrap";
 const AdminChatsPage = () => {
   return (
-    <div>AdminChatsPage</div>
-  )
-}
+    <Row className="m-5">
+      <Col md={2}>
+        <AdminLinksComponent />
+      </Col>
+      <Col md={10}>
+        <Row>
+          <AdminChatRoomComponent />
+        </Row>
+      </Col>
+    </Row>
+  );
+};
 
-export default AdminChatsPage
+export default AdminChatsPage;
